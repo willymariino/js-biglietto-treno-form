2bdd95f39ee02cb5438301f91d0b4df80f2e4bf8
willymariino <wg.mariino@gmail.com>
2025-03-05 16:47:10 +0100

    check milestone 2

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const newTaskElementName = document.getElementById("new-task-name")
 const newTaskElementAge = document.getElementById("new-task-age")
+const newtaskkilometers = document.getElementById('new-task-kilometers')
 const newTaskElementSend = document.getElementById("send-task")
 
-console.log(newTaskElementAge);
-console.log(newTaskElementAge);
-console.log(newTaskElementSend);
+
+console.log(newTaskElementName)
+console.log(newTaskElementAge)
+console.log(newtaskkilometers)
+console.log(newTaskElementSend)
 
 newTaskElementSend.addEventListener('click', function () {
     console.log('ho cliccato sul bottone')
     console.log(newTaskElementName.value)
     console.log(newTaskElementAge.value)
+    console.log(newtaskkilometers)
 })
+
+const price = newtaskkilometers * 0.21;
+let discount = 0;
+
+function ticketPrice(ages, kilometers) {
+    let ticketPrice = []
+    if (newTaskElementAge < 18) {
+        discount = price * 0.2
+    }
+    else if (newTaskElementAge > 65) {
+        discount = price * 0.4
+    }
+
+    return ticketPrice
+}
+
+const FinalPrice = price - discount
+console.log(FinalPrice.toFixed(2))
