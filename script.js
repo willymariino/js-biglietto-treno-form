603ae5c18e92f6c5f7c359355b9bc9e444f957f8
willymariino <wg.mariino@gmail.com>
2025-03-05 15:56:17 +0100

    added console log

---
+++ script.js
@@ -0,0 +1,7 @@
+const newTaskElementName = document.getElementById("new-task-name")
+const newTaskElementAge = document.getElementById("new-task-age")
+const newTaskElementSend = document.getElementById("send-task")
+
+console.log(newTaskElementAge);
+console.log(newTaskElementAge);
+console.log(newTaskElementSend);
